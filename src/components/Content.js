a8de1065a131adb4c30c54184f80b7bcb37f86d8
urstrulymahesh44 <kmrreddy44@gmail.com>
2022-02-17 11:10:34 +0530

    addded cards

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import { ReactComponent as Add } from '../assets/add.svg'
-
+import React from "react";
+import { ReactComponent as Add } from "../assets/add.svg";
+import Room from "./Room";
 const Content = () => {
   return (
-    <div className='content'>
-        <div className='contentHeader'>
-            <div className='contentheadwithtotal'>
-              <label className='contenthead'>
-                  Rooms
-              </label>
-              <p className='contenttotalrooms'>
-                34 Rooms available
-              </p>
-            </div>
-            <button> <Add className="add"></Add>Create Room</button>
+    <div className="content">
+      <div className="contentHeader">
+        <div className="contentheadwithtotal">
+          <label className="contenthead">Rooms</label>
+          <p className="contenttotalrooms">34 Rooms available</p>
         </div>
+        <button>
+          {" "}
+          <Add className="add"></Add>Create Room
+        </button>
+      </div>
+      <div className="rooms">
+        <Room />
+        <Room />
+        <Room />
+        <Room />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
---
+++ src/components/Room.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import studentm from '../assets/studentm.png';
+import studentw from '../assets/studentw.png';
+import { ReactComponent as People } from "../assets/people.svg"
+import user from '../assets/user.png'
+const Room = () => {
+  return (
+    <div className='room'>
+        <div className='owner'>
+            <div className='ownerheader'>
+                <img src={user} alt="" />
+                <div className='ownerdetail'>
+                    Mahesh
+                </div>
+            </div>
+            <p> 4 Weeks ago</p>
+        </div>
+        <div className='roomdetails'>
+            <label>Come Buddy let's Chat</label>
+            <div className='studentlist'>
+                <img src={studentm} alt="" />
+                <img src={studentw} alt="" />
+                <img src={studentw} alt="" />
+                <img src={studentm} alt="" />
+                <img src={studentw} alt="" />
+                <img src={studentm} alt="" />
+                <img src={studentm} alt="" />
+                <img src={studentw} alt="" />
+                <img src={studentm} alt="" />
+                <img src={studentw} alt="" />
+                <hr />
+            </div>
+            <div className='peoplejoined'>
+                <div className='number'>
+                    <People className="people" />
+                    210 joined
+                </div>
+                <label>Python</label>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Room

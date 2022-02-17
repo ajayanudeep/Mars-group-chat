2156f6939ecef3744a22123200847590e1ebecb6
urstrulymahesh44 <kmrreddy44@gmail.com>
2022-02-17 23:46:54 +0530

    Room headers added

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import backarrow from '../assets/backarrow.svg'
 const Chatroom = (props) => {
-    const title=useLocation();
     let {roomname} = useParams();
-    console.log(title);
   return (
       <div>
           <div className='chatroom'>
                 <div className='chatspace'> 
-                    <div className='header'>
-                        {roomname}
+                    <div className='chatspaceheader'>
+                        <div className='headercontent'>
+                            <Link to="/Home" className='link'><img src={backarrow} alt=""/></Link>
+                            <label htmlFor="">{roomname}</label>
+                        </div>
                     </div>
                 </div>
                 <div className='participants'>
